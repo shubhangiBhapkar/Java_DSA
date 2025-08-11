38435195ed3450721dd164fa3669ed85437db116
web Development <shubhangibhapkar35@gmail.com>
2025-08-12 01:17:47 +0530

    HashSet Iteration

--- Hashing/Iteration.java
+++ Hashing/Iteration.java
@@ -1,6 +1,19 @@
 import java.util.*;
 public class Iteration {
     public static void main(String[] args){
-        HashMap
+        HashMap<String,Integer> hm = new HashMap<>();
+         hm.put("India",100);
+        hm.put("china",203);
+        hm.put("USA",150);
+        hm.put("Indonesia",2150);
+        hm.put("Nepal",300);
+
+        Set<String> key = hm.keySet();
+        for (String k : key) {
+            System.out.println("key ="+k+ " value ="+hm.get(k));
+        }
+
+      
+
     }
 }
