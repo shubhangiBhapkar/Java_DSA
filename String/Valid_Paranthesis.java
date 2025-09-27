41cad86c6e4e33613e7db532f1e24eb52792ccf4
web Development <shubhangibhapkar35@gmail.com>
2025-09-27 23:58:00 +0530

    valid paranthesis

--- String/Valid_Paranthesis.java
+++ String/Valid_Paranthesis.java
@@ -1,12 +1,34 @@
+import java.util.Stack;
+
 public class Valid_Paranthesis {
-public boolean getValidParanthesis(String s){
-    Stack
+    public static boolean getValidParanthesis(String s) {
+        Stack<Character> stack = new Stack<>();
 
-    return true;
-}
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((top == '(' && ch == ')') ||
+                        (top == '{' && ch == '}') ||
+                        (top == '[' && ch == ']')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
 
-    public static void main(String args[]){
-        String s= "(){}[]";
+        }
+        return stack.isEmpty();
+
+    }
+
+    public static void main(String args[]) {
+        String s = "({})]";
         System.out.println(getValidParanthesis(s));
     }
 }
