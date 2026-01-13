b792c7365a5a0dda88df5a17667699c5ae332c7d
web Development <shubhangibhapkar35@gmail.com>
2026-01-13 22:41:56 +0530

    Longest Consecutive ones in array

---
+++ Array/ConsecutiveOnes.java
@@ -0,0 +1,21 @@
+public class ConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int count=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max,count);
+
+            }else{
+                count =0;
+            }
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        int nums[] = new int[]{1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
